4531a135c4b615bef60f4ed4ed895ccd6b6eaa29
ormequ <ilya.belokobylsky@gmail.com>
2023-08-04 14:46:16 +0300

    added composite primary key for link_ads

--- migrations/000004_create_link_ads_table.up.sql
+++ migrations/000004_create_link_ads_table.up.sql
@@ -2,5 +2,6 @@ CREATE TABLE link_ads
 (
     link_id BIGINT NOT NULL REFERENCES links (id) ON DELETE CASCADE,
     ad_id   BIGINT
-        CONSTRAINT link_ads_ad_id_key NOT NULL REFERENCES ads (id) ON DELETE CASCADE
+        CONSTRAINT link_ads_ad_id_key NOT NULL REFERENCES ads (id) ON DELETE CASCADE,
+    PRIMARY KEY (link_id, ad_id)
 );
